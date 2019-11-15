4791ff659f6359dc67b492aee21d320843a9db25
kimjh9434 <kimjh9434@gmail.com>
2019-11-15 18:27:24 +0900

    단계2 if문 if문을 사용해 봅시다.

---
+++ 백준/단계02_if문/4_2884_알람 시계.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	// 2019-10-08-화
+	// if문 4	2884	알람 시계 : https://www.acmicpc.net/problem/2884
+	// : 시간 뺄셈 문제 - 주어진 시간의 45분 전의 시간을 구하라
+
+	int H, M; cin >> H >> M;
+	M -= 45;
+	if (M < 0) {
+		H -= 1;
+		M += 60;
+		if (H < 0) H += 24;
+	}
+	cout << H << " " << M << endl;
+
+	return 0;
+}
