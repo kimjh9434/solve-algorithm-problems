ed1e88f0ef05cbafdabaaf0a380e5c7457e50a07
kimjh9434 <kimjh9434@gmail.com>
2019-11-15 18:48:22 +0900

    단계3 for문 for문을 사용해 봅시다.

---
+++ 백준/단계04_while문/1_10952_A+B - 5.cpp
@@ -0,0 +1,16 @@
+﻿#include <cstdio>
+
+int main(int argc, char* argv[])
+{
+	// 2019-10-08-화
+	// 단계4	while문 : 1	10952	A+B - 5 : https://www.acmicpc.net/problem/10952
+	// : 0 0이 들어올 때까지 A+B를 출력하는 문제 - 입력의 마지막에는 0 두 개가 들어온다.
+
+	int A = 1, B = 1;
+	while (1) {
+		scanf("%d %d", &A, &B);
+		if (A == 0 && B == 0) break;
+		printf("%d\n", A + B);
+	}
+	return 0;
+}
---
+++ 백준/단계04_while문/3_1110_더하기 사이클.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+
+	static public void main(String args[]) {
+        // Q3	1110	더하기 사이클
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		int cycleNum = 0; // 사이클 길이
+		int firstNum = N/10; // 10의자리 숫자
+		int secondNum = N%10;// 1의 자리 숫자
+		
+		int next_firstNum = secondNum; // 10의자리 숫자
+		int next_secondNum = (firstNum+secondNum)%10;// 1의 자리 숫자
+		cycleNum++;
+//		System.out.println(firstNum+""+secondNum);
+//		System.out.println(next_firstNum+""+next_secondNum);
+		
+		while(!(N/10==next_firstNum &&  N%10==next_secondNum)){
+			firstNum = next_firstNum;
+			secondNum = next_secondNum;
+			
+			next_firstNum = secondNum; 
+			next_secondNum = (firstNum+secondNum)%10;
+			cycleNum++;
+			
+//			System.out.println(firstNum+""+secondNum);
+//			System.out.println(next_firstNum+""+next_secondNum);
+		}
+		System.out.println(cycleNum);
+    }
+}
