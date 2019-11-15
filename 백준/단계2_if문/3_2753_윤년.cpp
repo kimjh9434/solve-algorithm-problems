1be78d9974c9249e131aa67351677318f2c3b120
kimjh9434 <kimjh9434@gmail.com>
2019-11-15 17:59:33 +0900

    단계2_ if문 : if문을 사용해 봅시다.

---
+++ 백준/단계2_if문/3_2753_윤년.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	// 2019-10-08-화
+	// 3	2753	윤년 : https://www.acmicpc.net/step/4
+	// : 윤년을 판별하는 문제 : 윤년이면 1, 아니면 0을 출력
+
+	// 윤년은 연도가 4의 배수이면서, 100의 배수가 아닐 때 또는 400의 배수일 때 이다.
+
+	int year; cin >> year;
+	bool isLeapYear = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)); // CF. 윤년 = leap year
+	cout << isLeapYear << endl;
+	return 0;
+}
