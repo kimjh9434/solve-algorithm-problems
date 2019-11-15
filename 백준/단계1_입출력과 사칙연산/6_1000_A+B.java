195d044b1f76513687d20941c5fafd8cc7b58075
kimjh9434 <kimjh9434@gmail.com>
2019-11-15 17:50:53 +0900

    단계1_입출력과 사칙연산 : 입력, 출력과 사칙연산을 연습해 봅시다. Hello World!

---
+++ 백준/단계1_입출력과 사칙연산/6_1000_A+B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+	// Q6_1000_A+B
+	static public void main(String args[]) {
+		//System.out.println("A+B문제");
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a + b);
+	}
